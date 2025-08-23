649318f4be278b428cc062363063fc72a15c78ff
billyleew <bcholeewong@gmail.com>
2025-08-22 21:19:38 -0300

    feat: change github account from  to

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,36 @@
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  let databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  let databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  });
+}
+
+export default status;
