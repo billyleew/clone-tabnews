a925ad18e0ca208c7570f5aaaed267d88f67742f
Billy Chun Ho Lee Wong <bcholeewong@gmail.com>
2026-02-01 02:36:33 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,29 +6,16 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("GET tp /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  /*
-  // Assert that specific migrations or schema changes exist
-  const tables = await database.query(`
-    SELECT table_name 
-    FROM information_schema.tables 
-    WHERE table_schema = 'public';
-  `);
-
-  // Check that tables expected from migrations are present
-  const tableNames = tables.rows.map((row) => row.table_name);
-  expect(tableNames).toEqual(expect.arrayContaining(["pgmigrations"]));
-
-  // Optionally check data integrity or initial inserts from migrations
-  const pgmigrations = await database.query(`SELECT * FROM pgmigrations`);
-  expect(pgmigrations.rows).toBeDefined();
-  */
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
